c006caa1d32424370410b04c2185292b8d70ed7f
Jeremy Smellie <jeremysmellie@gmail.com>
2015-12-05 11:26:01 -0500

    - Starting the logger over from scratch.  The current implementation was just too heavy and wasn't working the way I wanted it to. - Moved some things around in the Core folder for better folder structure. - Started converting all my line endings to LF to keep everything nice and cross-platform - Basic implementation of the new Service system is started.

---
+++ Core/Services/Logger/ILogger.cs
@@ -0,0 +1,40 @@
+/* --------------------------
+ *
+ * ILogger.cs
+ *
+ * Description: Interface for any loggers.  Used in Services.cs to hold a ref to the currently active logger.
+ *
+ * Author: Jeremy Smellie
+ *
+ * Editors:
+ *
+ * 12/5/2015 - Starvoxel
+ *
+ * All rights reserved.
+ *
+ * -------------------------- */
+
+#region Includes
+#region System Includes
+using System.Collections;
+#endregion
+
+#region Other Includes
+
+#endregion
+#endregion
+
+ namespace Starvoxel.Services
+{
+	public interface ILogger
+	{
+		#region Fields & Properties
+		//fields
+	
+		//properties
+		#endregion
+	
+		#region Methods
+		#endregion
+	}
+}
