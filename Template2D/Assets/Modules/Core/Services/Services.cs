34380222a44f8517d85ae10b2faa5ac0e1d6845b
Jeremy Smellie <jeremysmellie@gmail.com>
2015-12-05 11:26:01 -0500

    - Starting the logger over from scratch.  The current implementation was just too heavy and wasn't working the way I wanted it to. - Moved some things around in the Core folder for better folder structure. - Started converting all my line endings to LF to keep everything nice and cross-platform - Basic implementation of the new Service system is started.

---
+++ Template2D/Assets/Modules/Core/Services/Services.cs
@@ -0,0 +1,59 @@
+/* --------------------------
+ *
+ * Services.cs
+ *
+ * Description: Service pattern implementation.  Will be used for systems that need to be publicly available but can have different implementations based on various things.
+ *
+ * Author: Jeremy Smellie
+ *
+ * Editors:
+ *
+ * 12/5/2015 - Starvoxel
+ *
+ * All rights reserved.
+ *
+ * -------------------------- */
+
+#region Includes
+#region System Includes
+using System.Collections;
+#endregion
+
+#region Other Includes
+
+#endregion
+#endregion
+
+ namespace Starvoxel.Services
+{
+	public class Services
+	{
+		#region Fields & Properties
+		//const
+	
+		//public
+	
+		//protected
+	
+		//private
+	
+		//properties
+		#endregion
+	
+		#region Constructor Methods
+		public Services()
+		{
+			
+		}
+		#endregion
+	
+		#region Public Methods
+		#endregion
+	
+		#region Protected Methods
+		#endregion
+	
+		#region Private Methods
+		#endregion
+	}
+}
