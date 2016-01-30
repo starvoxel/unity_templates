603bcbbe09a84e4a00435c2b32101d5ef3fb001c
Jeremy Smellie <jeremysmellie@gmail.com>
2016-01-29 22:14:44 -0500

    Summary:  * Got placeholder Modal view functionality set up.                      * Made some placeholder scenes to test flow and actions and such                      * Added testing functionality to FlowManager so that you can fire any action anytime you want                      * Added a EditorCamera prefab so it's easy to get a EditorCamera set up and ready to go                      * EditorOnly script created.  On awake it destroys itself

--- FlowManagement/Scripts/ActionRunner.cs
+++ FlowManagement/Scripts/ActionRunner.cs
@@ -1,5 +1,35 @@
-/* -------------------------- * * ActionRunner.cs * * Description: Testing script used to fire actions * * Author: Jeremy Smellie * * Editors: * * 1/28/2016 - Starvoxel * * All rights reserved. * * -------------------------- */using UnityEngine;using System.Collections; namespace Starvoxel.FlowManagement{	public class ActionRunner : CustomMono	{		//const			//public			//protected        [SerializeField] private string m_ActionID;			//private			//properties        [ContextMenu("Run Action")]        private void ContextRunAction()
-        {
-            Debug.Log("Running Action: " + m_ActionID);
-            FlowManager.Instance.TriggerAction(m_ActionID);
-        }	}	}
+/* --------------------------
+ *
+ * ActionRunner.cs
+ *
+ * Description: Testing script used to fire actions
+ *
+ * Author: Jeremy Smellie
+ *
+ * Editors:
+ *
+ * 1/28/2016 - Starvoxel
+ *
+ * All rights reserved.
+ *
+ * -------------------------- */
+
+using UnityEngine;
+using System.Collections;
+
+ namespace Starvoxel.FlowManagement
+{
+	public class ActionRunner : CustomMono
+	{
+		//const
+	
+		//public
+	
+		//protected
+	
+		//private
+	
+		//properties
+	}
+	
+}
