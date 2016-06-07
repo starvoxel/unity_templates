4f52509d2148e411063543394c3dc7504905142d
Jeremy Smellie <jeremysmellie@gmail.com>
2016-06-06 22:50:12 -0400

    Squashed 'Template2D/Assets/Modules/' changes from def1dc0..18a9ce2
    18a9ce2 Summary:   * Started a simple "attack" behavior.  For now it's very similar to the simple chase behavior, but it will now stop X distance away from the target and rotate to always be looking at them.  Good start.
    40f8775 Summary:   * Took specific functionality out of the actual trees and put them in external trees to make it more reusable                       * The wanderer now has the same type of functionality as the patrol (health checking and all that)
    
    git-subtree-dir: Template2D/Assets/Modules
    git-subtree-split: 18a9ce2eccc063f0e617675e451085e1541c573e

---
+++ Core/CustomBehaviorTasks/AgentLookAt.cs
@@ -0,0 +1,83 @@
+/* --------------------------
+ *
+ * AgentLookAt.cs
+ *
+ * Description: Rotates the agent towards a specied object or location
+ *
+ * Author: Jeremy Smellie
+ *
+ * Editors:
+ *
+ * 4/2/2016 - Starvoxel
+ *
+ * All rights reserved.
+ *
+ * -------------------------- */
+
+#region Includes
+#region Unity Includes
+using UnityEngine;
+#endregion
+
+#region System Includes
+using System.Collections;
+#endregion
+
+#region Other Includes
+using Starvoxel.BiteMe;
+#endregion
+#endregion
+
+namespace BehaviorDesigner.Runtime.Tasks.Movement
+{
+	public class AgentLookAt : Action
+	{
+		#region Fields & Properties
+		//const
+
+        //public
+        [Tooltip("Target to look at")]
+        public SharedGameObject target;
+        [Tooltip("Location to look at.  Used if target is null.")]
+        public SharedVector3 targetLocation;
+        [Tooltip("Angular speed of the agent")]
+        public SharedFloat angularSpeed;
+	
+		//protected
+	
+		//private
+	
+		//properties
+		#endregion
+	
+		#region Public Methods
+        public override TaskStatus OnUpdate()
+        {
+            Vector2 targetPos = target.Value != null ? (Vector2)target.Value.transform.position : (Vector2)targetLocation.Value;
+            Vector2 wantedDirection = targetPos - (Vector2)this.transform.position;
+            wantedDirection.Normalize();
+
+            float rot = -Mathf.Atan2(wantedDirection.x, wantedDirection.y) * 180 / Mathf.PI;
+            float newZ = Mathf.MoveTowardsAngle(transform.localEulerAngles.z, rot, angularSpeed.Value * Time.deltaTime);
+
+            this.transform.localEulerAngles = new Vector3(this.transform.localEulerAngles.x, this.transform.localEulerAngles.y, newZ);
+
+            if (Mathf.Approximately(rot, newZ))
+            {
+                return TaskStatus.Success;
+            }
+            else
+            {
+                return TaskStatus.Running;
+            }
+        }
+		#endregion
+	
+		#region Protected Methods
+		#endregion
+	
+		#region Private Methods
+		#endregion
+	}
+	
+}
